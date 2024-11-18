9eed4c7dbece979e3a40b6db15cc9f52fab239b9
Pasan Diksura <pasan.20210415@iit.ac.lk>
2024-11-19 00:34:28 +0530

    Update ContentView.swift

--- Weather App/Weather App/ContentView.swift
+++ Weather App/Weather App/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var weatherData: WeatherDTO?
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(weatherData?.location?.country ?? "Unknown")")
         }
         .padding()
+        .onAppear {
+            Task {
+                await fetchWeatherData()
+            }
+        }
+    }
+    
+    func fetchWeatherData() async {
+        
+        // Creating URL
+        let url = URL(string: "https://api.weatherapi.com/v1/current.json?key=838714aecdf04acaad8173636241811&q=6.927079,79.861244&aqi=no")
+        guard let unwrappedURL = url else {
+            return
+        }
+        
+        do {
+            // initialize URL session
+            let (data, response) = try await URLSession.shared.data(from: unwrappedURL)
+            
+            // check if valis responce
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("invalid response")
+                return
+            }
+            
+            switch httpResponse.statusCode {
+            case 200..<300:
+                let decodeData = try JSONDecoder().decode(WeatherDTO.self, from: data)
+                weatherData = decodeData
+                
+                print("Successfully Fetched Data")
+            case 400..<500:
+                print("Invalis Request")
+            default:
+                print("Error Fetching Data")
+            }
+            
+
+        } catch {
+            print("Error Fetching Data: \(error.localizedDescription)")
+        }
+        
+        
+        
+        
     }
 }
 
