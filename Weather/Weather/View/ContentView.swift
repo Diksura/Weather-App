95de3f793153ee306d1ec7ce8d432769c971fc00
Pasan Diksura <pasan.20210415@iit.ac.lk>
2024-11-22 17:49:53 +0530

    Update ContentView.swift

--- Weather/Weather/View/ContentView.swift
+++ Weather/Weather/View/ContentView.swift
@@ -16,16 +16,56 @@ struct ContentView: View {
     
 
     var body: some View {
-        VStack {
-            if let location = locationService.location {
-                
-                Image(systemName: "cloud")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                
-                Text("\(weatherData?.location.name ?? "Unknown")")
-                
-                Text("Latitude: \(location.latitude), Longitude: \(location.longitude)")
+        
+        NavigationStack {
+            
+            if let _ = locationService.location {
+                ScrollView {
+                    VStack {
+                        
+                        Spacer()
+                        
+                        VStack{
+                            VStack(spacing: 0) {
+                                Image("Cloudy-Sunny")
+                                    .resizable()
+                                    .frame(width: 300, height: 300)
+                            
+
+                                Text("\(weatherData?.current.tempC ?? 0, specifier: "%.0f")ºC")
+                                    .font(.system(size: 102.0))
+                //                    .fontWeight(.bold)
+                                    .fontDesign(.rounded)
+                            }
+
+                            Text("\(weatherData?.location.name ?? "Unknown Location")")
+                                .font(.title)
+            //                    .fontWeight(.bold)
+                            
+                            Text("\(weatherData?.current.condition.text ?? "Unknown Weather")")
+                        }
+                        
+                        Spacer()
+                        
+                        Image(systemName: "platter.filled.bottom.and.arrow.down.iphone")
+                            .padding([.bottom], 1)
+                            .foregroundStyle(.gray)
+                        
+                        Text("Scroll Down for More")
+                            .font(.caption2)
+                            .foregroundStyle(.gray)
+                    }
+                    .frame(width: .infinity, height: UIScreen.main.bounds.height - 125)
+                    
+                    
+                    VStack {
+                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                    }
+                    .padding(.vertical, 40)
+                    
+                }
+                .scrollIndicators(.hidden)
+
             
             } else {
                 Text("Fetching location...")
