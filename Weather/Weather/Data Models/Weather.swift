4d6204891d4de7c4cbd7bbd5a912278a859a95aa
Pasan Diksura <pasan.20210415@iit.ac.lk>
2024-11-23 16:12:31 +0530

    Update Weather.swift

--- Weather/Weather/Data Models/Weather.swift
+++ Weather/Weather/Data Models/Weather.swift
@@ -104,3 +104,25 @@ struct ConditionDTO: Decodable, Hashable {
     let icon: String
     let code: Int
 }
+
+struct AirQualityDTO: Decodable, Hashable {
+    let co: Double
+    let no2: Double
+    let o3: Double
+    let so2: Double
+    let pm2_5: Double
+    let pm10: Double
+    let usEpaIndex: Double
+    let gbDefraIndex: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case co
+        case no2
+        case o3
+        case so2
+        case pm2_5
+        case pm10
+        case usEpaIndex = "us-epa-index"
+        case gbDefraIndex = "gb-defra-index"
+    }
+}
