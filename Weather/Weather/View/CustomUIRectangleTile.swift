57cfa470f650da5144aa03a6701c0f898a31a0ee
Pasan Diksura <pasan.20210415@iit.ac.lk>
2024-11-25 00:01:45 +0530

    Create CustomUIRectangleTile.swift

---
+++ Weather/Weather/View/CustomUIRectangleTile.swift
@@ -0,0 +1,48 @@
+//
+//  CustomUIRectangleTile.swift
+//  Weather
+//
+//  Created by Pasan Diksura on 2024-11-24.
+//
+
+import SwiftUI
+
+struct CustomUIRectangleTile<TileContent: View>: View {
+    
+    @Binding var tileTitle: String
+    var height: CGFloat?
+    
+    @ViewBuilder var tileContent: () -> TileContent
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("\(tileTitle)")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 15)
+    //                .padding(.bottom, 5)
+                .padding(.horizontal, 15)
+            
+            Divider()
+                .padding(.horizontal, 10)
+            
+            Spacer()
+            
+            tileContent()
+        }
+        .frame(width: Constants().uiRectangleWidth, height: height)
+        .frame(minHeight: Constants().uiSquareSize)
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(15)
+    }
+    
+}
+
+#Preview {
+    CustomUIRectangleTile(tileTitle: .constant("Tile Title"), height: Constants().uiRectangleWidth) {
+        Text("This is the tile content")
+            .font(.headline)
+            .foregroundColor(.blue)
+    }
+}
