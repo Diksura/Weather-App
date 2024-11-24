5dc5889f46d3a7ba03fb93df8bfeb75ec3aaeb24
Pasan Diksura <pasan.20210415@iit.ac.lk>
2024-11-25 00:01:39 +0530

    Update Constants.swift

--- Weather/Weather/Constants/Constants.swift
+++ Weather/Weather/Constants/Constants.swift
@@ -5,8 +5,16 @@
 //  Created by Pasan Diksura on 2024-11-20.
 //
 
-import Foundation
+import SwiftUI
 
 class Constants {
+    /// API Key
     static let apiKey = "838714aecdf04acaad8173636241811"
+    
+    /// View Constrains
+    let uiSquareSize: CGFloat = (CGFloat(UIScreen.main.bounds.width) - 60) / 2
+    let uiRectangleWidth: CGFloat = (CGFloat(UIScreen.main.bounds.width) - 40)
+
+    
 }
+
