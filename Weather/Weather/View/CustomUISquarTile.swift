b9d4069760d33bd5a71f3869018d725fad553242
Pasan Diksura <pasan.20210415@iit.ac.lk>
2024-11-25 00:01:47 +0530

    Create CustomUISquarTile.swift

---
+++ Weather/Weather/View/CustomUISquarTile.swift
@@ -0,0 +1,44 @@
+//
+//  CustomUISquarTile.swift
+//  Weather
+//
+//  Created by Pasan Diksura on 2024-11-24.
+//
+
+import SwiftUI
+
+struct CustomUISquarTile<TileContent: View>: View {
+    
+    @Binding var tileTitle: String
+    @ViewBuilder var tileContent: () -> TileContent
+
+    var body: some View {
+        VStack {
+            Text("\(tileTitle)")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 15)
+                .padding(.bottom, 15)
+                .padding(.horizontal, 15)
+
+            
+            Spacer()
+            
+            tileContent()
+        }
+        .frame(width: Constants().uiSquareSize, height: Constants().uiSquareSize)
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(15)
+    }
+    
+}
+
+#Preview {
+    CustomUISquarTile(tileTitle: .constant("Tile Title")) {
+        Text("This is the tile content")
+            .font(.headline)
+            .foregroundColor(.blue)
+    }
+}
+
