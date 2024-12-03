c3540dceed501c967155db1247373cf30aa67ed6
Pasan Diksura <pasan.20210415@iit.ac.lk>
2024-12-03 23:04:42 +0530

    Update Constants.swift

--- Weather/Weather/Constants/Constants.swift
+++ Weather/Weather/Constants/Constants.swift
@@ -12,8 +12,8 @@ class Constants: ObservableObject {
     static let apiKey = "838714aecdf04acaad8173636241811"
     
     /// Settings
-    @Published  var isCelecious: Bool = true
-    @Published  var isSpeedKPH: Bool = true
+    @Published var isCelecious: Bool = true
+    @Published var isSpeedKPH: Bool = true
     @Published var isDistanceKm: Bool = true
     @Published var isLanguageEng: Bool = true
     @Published var isPrecipitationMM: Bool = true
