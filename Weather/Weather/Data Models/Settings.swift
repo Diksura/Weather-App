1978dac5818917561cc7fdd64578f076dbc8b743
Pasan Diksura <pasan.20210415@iit.ac.lk>
2025-01-24 22:38:28 +0530

    Create Settings.swift

---
+++ Weather/Weather/Data Models/Settings.swift
@@ -0,0 +1,23 @@
+//
+//  Settings.swift
+//  Weather
+//
+//  Created by Pasan Diksura on 2025-01-24.
+//
+
+import Foundation
+
+class Settings {
+    
+     var isCelecious: Bool = true
+     var isSpeedKPH: Bool = true
+     var isDistanceKm: Bool = true
+     var isLanguageEng: Bool = true
+     var isPrecipitationMM: Bool = true
+     var isAirUKDefra: Bool = true
+    
+     var pressureUnitType: PressureUnits = PressureUnits.kPa
+     var languageType: Language = Language.English
+    
+    
+}
