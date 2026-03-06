6cf4da8353f619202a7ee4c277e24fb63d04e1a3
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-03-06 12:44:48 +0530

    Create WeatherController.cs

---
+++ WeatherServer/WeatherServer.API/Controllers/WeatherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherServer.API.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
