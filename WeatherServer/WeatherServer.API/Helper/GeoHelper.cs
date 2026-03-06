5ea6f25ce41f12249095d00527544d8bbf54425c
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-03-07 02:19:11 +0530

    Create GeoHelper.cs

---
+++ WeatherServer/WeatherServer.API/Helper/GeoHelper.cs
@@ -0,0 +1,12 @@
+﻿using NGeoHash;
+
+    namespace WeatherServer.API.Helper
+{
+    public static class GeoHelper
+    {
+        public static string GetGeoHash(double latitude, double longitude)
+        {
+            return GeoHash.Encode(latitude, longitude, 6);
+        }
+    }
+}
