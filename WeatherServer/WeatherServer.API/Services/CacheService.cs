11fef4dc4e91bd6e52619ba79e212bd96e3c1541
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-03-07 02:19:09 +0530

    Create CacheService.cs

---
+++ WeatherServer/WeatherServer.API/Services/CacheService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace WeatherServer.API.Services
+{
+    public class CacheService
+    {
+
+        private readonly IMemoryCache _cache;
+
+        public CacheService(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public T? Get<T>(string key)
+        {
+            _cache.TryGetValue(key, out T? value);
+            return value;
+        }
+
+
+        public void Set<T>(string key, T value, TimeSpan timeSpan)
+        {
+            var options = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = timeSpan
+            };
+
+            _cache.Set(key, value, options);
+        }
+
+    }
+}
