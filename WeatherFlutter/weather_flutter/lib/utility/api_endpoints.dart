e51b144a95fcf25edfacdaf37a141b6623f5706b
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-03-15 08:33:34 +0530

    Update api_endpoints.dart

--- WeatherFlutter/weather_flutter/lib/utility/api_endpoints.dart
+++ WeatherFlutter/weather_flutter/lib/utility/api_endpoints.dart
@@ -1,5 +1,14 @@
 class ApiEndpoints {
 
+  static const scheme = 'https';
+  static const host = 'weather-api-541061882281.asia-south1.run.app';
+  static const port = null; // Cloud Run uses default HTTPS port
+  static const basePath = '/api/weather';
+
+}
+
+class ApiEndpointsLocal {
+
   static const scheme = 'http';
   static const host = '10.0.2.2'; // for emulator
   static const port = 5031;
