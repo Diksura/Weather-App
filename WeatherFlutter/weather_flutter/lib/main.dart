affb2065c439f0e5dbaee4cf0207fbe492c7778d
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-05-12 09:54:32 +0530

    System

--- WeatherFlutter/weather_flutter/lib/main.dart
+++ WeatherFlutter/weather_flutter/lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         // colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
+      debugShowCheckedModeBanner: false,
       home: const LoadingPage(),
     );
   }
